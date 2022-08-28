305544b6b984c816dea47aa109cdca5ad9a7630b
Mugdha Kshirsagar <91776088+mugdhaK21@users.noreply.github.com>
2022-08-28 21:00:29 +0530

    Time: 11 ms (68.99%), Space: 7.2 MB (74.60%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    
+public:
+    bool checkInclusion(string s1, string s2) {
+                int count1[26]={0};
+        for(int i=0;i<s1.length();i++){
+            int index = s1[i]-'a';
+            count1[index]++;
+            
+        }
+        //traverse s2 string by using a window of size s1 string and compare
+        int i=0;
+        int windowSize = s1.length();
+        int count2[26] = {0};
+        while(i<windowSize && i<s2.length()){
+            int index = s2[i]-'a';
+            count2[index]++;
+            i++;
+            
+        }
+        if(checkEqual(count1,count2))
+            return 1;
+        
+        //aage window process karo
+        
+        while(i<s2.length()){
+            char newChar = s2[i];
+            int index = newChar-'a';
+            count2[index]++;
+            char oldChar = s2[i-windowSize];
+            index = oldChar-'a';
+            count2[index]--;
+            i++;
+            
+            if(checkEqual(count1,count2))
+                    return 1;
+        
+            
+     }
+        return 0;
+
+    }
+    
+      bool checkEqual(int a[26],int b[26]){
+          for(int i=0;i<26;i++){
+              if(a[i]!= b[i])
+                  return 0;
+            
+          }
+          return 1;
+      }
+};
