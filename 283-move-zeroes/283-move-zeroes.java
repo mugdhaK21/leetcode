5a9be4b02af21e7fc914b64802b321fe26d8a7c5
Mugdha Kshirsagar <91776088+mugdhaK21@users.noreply.github.com>
2022-09-22 19:42:15 +0530

    Time: 10 ms (16.78%), Space: 55.4 MB (9.26%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[j]!=0){
+                nums[i++]=nums[j];
+            }
+            j++;
+        }
+        
+        while(i<nums.length){
+            nums[i++]=0;
+        }
+    }
+}
