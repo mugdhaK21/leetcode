5aa129e33966dcc73c12976e11de4033d62e1514
Mugdha Kshirsagar <91776088+mugdhaK21@users.noreply.github.com>
2022-09-28 18:11:15 +0530

    Time: 3 ms (41.25%), Space: 5.9 MB (24.23%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        for(int i = -200;i<=200;i++){
-            if(num1+num2 == i){
-                
-                return i;
-            }
-        }
-        return 0;
+        return num1+num2;
     }
 };
