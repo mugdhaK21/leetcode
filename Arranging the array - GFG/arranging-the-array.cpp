cb9158412d38ac71068e0918d828dba14c697064
Mugdha Kshirsagar <91776088+mugdhaK21@users.noreply.github.com>
2023-06-10 12:00:23 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            queue<int>q;
+            int i=0,j=0;
+            while(i<n){
+                
+                if(arr[i]<0){
+                    arr[j++]=arr[i];
+                }
+                else{
+                    q.push(arr[i]);
+                }
+                i++; }
+                
+            while(!q.empty()){
+                arr[j++]=q.front();
+                q.pop();
+            }
+            
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
